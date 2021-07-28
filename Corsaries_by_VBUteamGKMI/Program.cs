fe518b67035b1961ba57b4c4af5553913eee8a5c
Dice_kl17 <Dice_kl17@student.itstep.org>
2021-07-28 16:57:58 +0300

    Добавьте файлы проекта.

---
+++ Corsaries_by_VBUteamGKMI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Corsaries_by_VBUteamGKMI
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
